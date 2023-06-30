a5cb914a036753d0fc8b54f165fe28e1f1d2a6df
caleb <calebfelix38@gmail.com>
2023-06-30 18:16:59 +0530

    version update

--- CodeVerse_api/index.js
+++ CodeVerse_api/index.js
@@ -7,7 +7,9 @@ const morgan = require("morgan");
 dotenv.config();
 const app = express();
 
-
+app.get('/',(req,res)=>{
+    res.send({"Api":"CodeVerse","version":"1.0.1"})
+})
 
 app.listen(8000, () => {
     console.log("backend server running....")
