29223248b02ae5f198b687e95982b937fe703360
caleb <calebfelix38@gmail.com>
2023-07-03 20:26:49 +0530

    mongodb connection added

--- CodeVerse_api/index.js
+++ CodeVerse_api/index.js
@@ -1,16 +1,32 @@
+//IMPORTS
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB = require("./db");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
-dotenv.config();
-const app = express();
+//CONFIGS
+dotenv.config(); // environment variables
+const app = express(); //mainserver
+connectDB(); //database connection
+port = process.env.PORT || 8800; //connection port
 
-app.get('/',(req,res)=>{
-    res.send({"Api":"CodeVerse","version":"1.0.1"})
-})
+//MIDDLEWARES
+app.use(express.json()); //bodyparser
+app.use(helmet());
+app.use(morgan("common"));
 
-app.listen(8000, () => {
-    console.log("backend server running....")
-})
+//API ROUTES
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+  res.send({ Api: "CodeVerse", version: "1.0.1" });
+});
+
+app.listen(port, () => {
+  console.log(`backend server running on http://localhost:${port}`);
+});
---
+++ CodeVerse_api/routes/users.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+// update user
+// delete user
+// get a user
+// follow a user user
+// unfollow a user user
+
+module.exports = router
