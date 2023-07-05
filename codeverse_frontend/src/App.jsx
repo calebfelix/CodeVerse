575198c97066398e4a25ffc36c9515f21ca38301
caleb <calebfelix38@gmail.com>
2023-07-05 21:06:47 +0530

    Codeverse frontend project created and setup

---
+++ codeverse_frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/pages/Home";
+import { Person } from "@mui/icons-material";
+
+function App() {
+  return (
+    <>
+      <Person />
+      <Home />
+      <div>this is app</div>
+    </>
+  );
+}
+export default App;
